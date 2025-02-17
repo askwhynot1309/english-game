a69d69d2158af57a8d7cde0289c60bfc2072664e
AskWhyNot <tommarvoloriddle1975@gmail.com>
2025-02-17 22:40:35 +0700

    update more screen

--- english-game-unity/Assets/Script/GameManager.cs
+++ english-game-unity/Assets/Script/GameManager.cs
@@ -16,6 +16,9 @@ public class GameManager : MonoBehaviour
     public Button answer2Button;
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI timerText;
+    public GameObject gameOverPanel;
+    public TextMeshProUGUI finalScoreText;
+    public TextMeshProUGUI feedbackText;
 
     private List<WordData> words = new List<WordData>
     {
@@ -27,8 +30,23 @@ public class GameManager : MonoBehaviour
         new WordData("computer", "phone"),
         new WordData("house", "building"),
         new WordData("fish", "bird"),
-        new WordData("tree", "bush"),
+        new WordData("green tree", "bush"),
         new WordData("pencil", "pen"),
+        new WordData("bookshelf", "table"),
+        new WordData("cat", "bear"),
+        new WordData("shoes", "sandals"),
+        new WordData("cup", "mug"),
+        new WordData("crayon", "chalk"),
+        new WordData("truck", "van"),
+        new WordData("paper", "rock"),
+        new WordData("tent", "factory"),
+        new WordData("Formula 1", "NASCAR"),
+        new WordData("Umbrella", "Triangle"),
+        new WordData("Treasure map", "book"),
+        new WordData("rose flower", "tree"),
+        new WordData("chicken", "bird"),
+        new WordData("school bag", "suitcase"),
+        new WordData("hat", "pants"),
     };
     private int score = 0;
     private float timeRemaining = 30f;
@@ -37,10 +55,13 @@ public class GameManager : MonoBehaviour
     private string correctAnswer;
     private const string API_KEY = "0jjEcn9CHDFAk5GvSM0nISvfbjmEv7FfLFndfzKdF4cozSyr9e8xw7hM";
     private const string PEXELS_API_URL = "https://api.pexels.com/v1/search?query=";
+    private int highScore = 0;
 
     void Start()
     {
         StartNewRound();
+        gameOverPanel.SetActive(false);
+        highScore = PlayerPrefs.GetInt("HighScore", 0);
         answer1Button.onClick.AddListener(() => CheckAnswer(answer1Text.text));
         answer2Button.onClick.AddListener(() => CheckAnswer(answer2Text.text));
     }
@@ -54,10 +75,25 @@ public class GameManager : MonoBehaviour
 
             if (timeRemaining <= 0)
             {
-                gameActive = false;
-                timerText.text = "Game Over!";
+                timerText.text = "Time's up!";
+                GameOver();
             }
         }
+    }
+
+    void GameOver()
+    {
+        gameActive = false;
+        gameOverPanel.SetActive(true);
+
+        if (score > highScore)
+        {
+            highScore = score;
+            PlayerPrefs.SetInt("HighScore", highScore);
+            PlayerPrefs.Save();
+        }
+
+        finalScoreText.text = $"Final Score: {score}\nHigh Score: {highScore}";
     }
 
     void StartNewRound()
@@ -134,8 +170,36 @@ public class GameManager : MonoBehaviour
         {
             score += 100;
             scoreText.text = "Score: " + score;
+            feedbackText.text = "Correct";
+            feedbackText.color = Color.green;
+        }
+        else
+        {
+            feedbackText.text = "Incorrect!";
+            feedbackText.color = Color.red;
         }
 
+        feedbackText.alpha = 1;
+        StartCoroutine(HideFeedbackAfterDelay());
+
         StartNewRound();
+    }
+
+    public void RestartGame()
+    {
+        score = 0;
+        timeRemaining = 30f;
+        gameActive = true;
+        gameOverPanel.SetActive(false);
+        scoreText.text = "Score: " + score;
+        timerText.text = "Time: " + Mathf.Ceil(timeRemaining);
+        Debug.Log("Restart Game");
+        StartNewRound();
+    }
+
+    IEnumerator HideFeedbackAfterDelay()
+    {
+        yield return new WaitForSeconds(0.5f);
+        feedbackText.alpha = 0;
     }
 }
---
+++ english-game-unity/Assets/Script/WordData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class WordData
+{
+    public string correctWord;
+    public string wrongWord;
+
+    public WordData(string correct, string wrong)
+    {
+        correctWord = correct;
+        wrongWord = wrong;
+    }
+}
